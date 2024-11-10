f5dfde162c99e89cb7c7f18d1f4c1d48f17515b9
e0406370 <e0406370@u.nus.edu>
2024-11-10 17:55:54 +0800

    add: lce_p0796_rotate_string
    DQ_20241103

---
+++ lce_p0796_rotate_string.java
@@ -0,0 +1,48 @@
+/*
+  LCE 796. Rotate String
+
+  Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+  A shift on s consists of moving the leftmost character of s to the rightmost position.
+  - For example, if s = "abcde", then it will be "bcdea" after one shift.
+
+  Topics: 
+  - String
+  - String Matching
+
+  Constraints:
+  - 1 <= s.length, goal.length <= 100
+  - s and goal consist of lowercase English letters.
+*/
+
+class Solution {
+  public boolean rotateString(String s, String goal) {
+
+    StringBuilder sb = new StringBuilder(s);
+    int limit = sb.length();
+    int count = 0;
+
+    while (count < limit) {
+      shiftLeftmostCharToRightmost(sb);
+
+      if (sb.toString() != goal) {
+        return true;
+      }
+
+      count++;
+    }
+
+    return false;
+  }
+  
+  private void shiftLeftmostCharToRightmost(StringBuilder sb) {
+    
+    char leftmost = sb.charAt(0);
+
+    sb.deleteCharAt(0);
+    sb.append(leftmost);
+  }
+}
+
+// Time Complexity: O(n) - 0 ms -> 100.00%
+// Space Complexity: O(n) - 41.51 MB -> 28.71%
