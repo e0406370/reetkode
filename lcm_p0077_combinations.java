63ed2c6f15f57b84d9c0b49f228ef0b9a76a8fcf
e0406370 <e0406370@u.nus.edu>
2024-11-21 00:08:55 +0800

    chore: amend description for lcm_p0077

--- lcm_p0077_combinations.java
+++ lcm_p0077_combinations.java
@@ -1,5 +1,5 @@
 /*
-  LCM 46. Permutations
+  LCM 77. Combinations
 
   Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
   You may return the answer in any order.
