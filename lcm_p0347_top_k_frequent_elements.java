86b7405a81cf390854901b9066295a8b2d6444ed
e0406370 <e0406370@u.nus.edu>
2024-11-23 19:33:59 +0800

    review: include bucket sort mtd for lcm_p0347
    NC150_20241123

--- lcm_p0347_top_k_frequent_elements.java
+++ lcm_p0347_top_k_frequent_elements.java
@@ -21,6 +21,8 @@
 */
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -29,7 +31,7 @@ class Solution {
   // Time Complexity: O(n log n) - 13 ms -> 76.11%
   // Space Complexity: O(n) - 48.6 MB -> 52.11%
   public int[] topKFrequent(int[] nums, int k) {
-    
+
     Map<Integer, Integer> freqMap = new HashMap<>();
     for (int num : nums) {
       freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
@@ -44,7 +46,41 @@ class Solution {
     for (int i = 0; i < k; i++) {
       res[i] = queue.poll().getKey();
     }
-    
+
+    return res;
+  }
+
+  // Time Complexity: O(n) - 12 ms -> 82.75%
+  // Space Complexity: O(n) - 50.15 MB -> 5.35%
+  public int[] topKFrequentAlt(int[] nums, int k) {
+
+    int n = nums.length;
+
+    Map<Integer, Integer> freqMap = new HashMap<>();
+    for (int num : nums) {
+      freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+    }
+
+    List<Integer>[] buckets = new List[n + 1]; // create (n + 1) buckets; +1 since arrays are zero-indexed
+    for (int i = 0; i < n + 1; i++) {
+      buckets[i] = new LinkedList<>();
+    }
+
+    for (Map.Entry<Integer, Integer> e : freqMap.entrySet()) {
+      buckets[e.getValue()].add(e.getKey()); // buckets are categorised by the frequency of the num
+    }
+
+    int[] res = new int[k];
+    for (int resIdx = 0, buckIdx = n; resIdx < k && buckIdx >= 0; buckIdx--) {
+      for (int buck : buckets[buckIdx]) {
+        res[resIdx++] = buck;
+      }
+
+      if (resIdx == k) {
+        return res;
+      }
+    }
+
     return res;
   }
 }
@@ -52,4 +88,10 @@ class Solution {
 /*
   methods:
   1. priority queue - decreasing frequency
+  2. bucket sort algorithm - only grouping into buckets based on frequency; sorting within each bucket is not required
+
+  note for 2.
+  - each element in nums is processed only once, either when added to a bucket or when extracted from it
+  - outer loop iterates over the buckets
+  - inner loop iterates over the elements in a specific bucket
 */
