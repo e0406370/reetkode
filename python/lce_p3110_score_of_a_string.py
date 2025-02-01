c4078a4f4551a64743e6adcb9753a3f7b2e0c0e4
e0406370 <e0406370@u.nus.edu>
2025-02-02 00:29:21 +0800

    [add][py] lce_p3110_score_of_a_string

---
+++ python/lce_p3110_score_of_a_string.py
@@ -0,0 +1,24 @@
+"""
+  LCE 3110. Score of a String
+
+  You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+  Return the score of s.
+
+  Constraints:
+  - 2 <= s.length <= 100
+  - s consists only of lowercase English letters.
+
+  Topics:
+  - String
+"""
+
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+
+        return score
