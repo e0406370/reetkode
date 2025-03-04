d3e24733bec6e6dbd0d0b7c55abc04ae87b19e78
e0406370 <e0406370@u.nus.edu>
2025-03-04 23:05:31 +0800

    [rev][py] lce_p1800 - add Kadane's Algorithm

--- python/lce_p1800_maximum_ascending_subarray_sum.py
+++ python/lce_p1800_maximum_ascending_subarray_sum.py
@@ -18,6 +18,9 @@
 
 
 class Solution:
+    
+    # Time Complexity: O(n) - 0 ms -> 100.00%
+    # Space Complexity: O(1) - 17.66 MB -> 79.11%
     def maxAscendingSum(self, nums: List[int]) -> int:
         max_sum = 0
         curr_sum = nums[0]
@@ -33,5 +36,27 @@ class Solution:
         return max(curr_sum, max_sum)
 
 
-# Time Complexity: O(n) - 0 ms -> 100.00%
-# Space Complexity: O(1) - 17.66 MB -> 79.11%
+    # Time Complexity: O(n) - 0 ms -> 100.00%
+    # Space Complexity: O(1) - 17.96 MB -> 8.49%
+    def maxAscendingSumAlt(self, nums: List[int]) -> int:
+        maxEnding = nums[0]
+        res = nums[0]
+
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                maxEnding = nums[i]
+                continue
+
+            maxEnding = max(maxEnding, maxEnding + nums[i])
+
+            res = max(maxEnding, res)
+
+        return res
+
+
+"""
+  methods:
+  1. standard linear expansion
+  2. Kadane's Algorithm
+"""
+
