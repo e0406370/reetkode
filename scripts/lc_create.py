69fc271b0538cacba0d2197636ac3b5d92ffd872
e0406370 <e0406370@u.nus.edu>
2025-05-15 10:47:53 +0800

    refac: lc_create

--- scripts/lc_create.py
+++ scripts/lc_create.py
@@ -2,26 +2,27 @@ import os
 import sys
 
 
-def create_leetcode(type, difficulty, title):
-  
+def create_leetcode(filetype: str, difficulty: str, title: str) -> None:
+
     valid_difficulties = ["easy", "medium", "hard"]
     if difficulty not in valid_difficulties:
         print(f"Error: Difficulty must be one of {valid_difficulties}.")
         sys.exit(1)
 
-    number = title.split(".")[0].zfill(4)
-    text = title.split(".")[1].strip()
+    tokens = title.split(".")
+    number = tokens[0].zfill(4)
+    text = tokens[1].strip()
 
     name = f"lc{difficulty[0]}_p{number}_{text.replace(' ', '_').lower()}"
     try:
-        if type == "folder":
+        if filetype == "folder":
             os.makedirs(name, exist_ok=True)
             print(f"Folder created: {name}")
 
         else:
-            name = name + (f".{type}")
-            open(name, "x")
-            print(f"File created: {name}")
+            name = f"{name}.{filetype}"
+            with open(name, "x"):
+                print(f"File created: {name}")
 
     except Exception as e:
         print(f"Error: {e}")
@@ -29,13 +30,13 @@ def create_leetcode(type, difficulty, title):
 
 
 if __name__ == "__main__":
-  
+
     if len(sys.argv) < 4:
-        print("Usage: python lc_create.py <type> <difficulty> <title>")
+        print("Usage: python lc_create.py <filetype> <difficulty> <title>")
         sys.exit(1)
 
-    type = sys.argv[1]
+    filetype = sys.argv[1]
     difficulty = sys.argv[2]
     title = " ".join(sys.argv[3:])
 
-    create_leetcode(type, difficulty, title)
+    create_leetcode(filetype, difficulty, title)
