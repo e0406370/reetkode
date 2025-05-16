74dedd459a351050cdad082c81f27f3abd9c5121
e0406370 <e0406370@u.nus.edu>
2025-05-17 00:03:44 +0800

    refac: include totals in lc_stats

--- scripts/lc_stats.py
+++ scripts/lc_stats.py
@@ -1,36 +1,51 @@
+from collections import defaultdict
 import pathlib
 
+STATS_KEYS = ["easy", "medium", "hard", "total"]
+EASY, MEDIUM, HARD, TOTAL = STATS_KEYS
 
-IGNORED = [".git", "scripts"]
+IGNORED_DIRS = [".git", "scripts"]
 
 
 def stats_leetcode() -> None:
 
     base_dir = pathlib.Path(__file__).parent.parent.resolve()
+    total_stats: dict[str, dict[str, int]] = defaultdict(dict)
+
     for sub_dir in base_dir.iterdir():
-        if sub_dir.name not in IGNORED:
+        language = sub_dir.name
+        if language not in IGNORED_DIRS:
             stats = {
-              "easy": 0,
-              "medium": 0,
-              "hard": 0
+                EASY: 0,
+                MEDIUM: 0,
+                HARD: 0,
+                TOTAL: 0
             }
 
             for solution in sub_dir.iterdir():
-                match solution.name[:3]:
-                  case "lce":
-                    stats["easy"] += 1
-                  case "lcm":
-                    stats["medium"] += 1
-                  case "lch":
-                    stats["hard"] += 1
+                stats[TOTAL] += 1
 
-            statement = (
-                f"{sub_dir.name.capitalize()}\n"
-                + "\n".join(f"{k}: {v}" for k, v in stats.items())
-                + "\n"
-                + f"total: {sum(stats.values())}"
-            )
-            print(f"\n{statement}\n")
+                match solution.name[:3]:
+                    case "lce":
+                        stats[EASY] += 1
+                    case "lcm":
+                        stats[MEDIUM] += 1
+                    case "lch":
+                        stats[HARD] += 1
+
+            total_stats[language] = stats
+
+    total_stats[TOTAL] = {
+        key: sum(stats[key] for stats in total_stats.values())
+        for key in STATS_KEYS
+    }
+
+    for language, stats in total_stats.items():
+        statement = (
+            f"{language.capitalize()}\n"
+            + "\n".join(f"{k}: {v}" for k, v in stats.items())
+        )
+        print(f"\n{statement}\n")
 
 
 if __name__ == "__main__":
