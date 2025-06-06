c0cbf02d458387e9b42b40c75a2ccbb3038a052e
e0406370 <e0406370@u.nus.edu>
2025-06-06 18:57:43 +0800

    [rev][java] 1 LCE + 2 LCM - 242, 49, 128
    from previous dir

--- java/lcm_p0049_group_anagrams.java
+++ java/lcm_p0049_group_anagrams.java
@@ -4,7 +4,7 @@
   Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 
   Constraints:
-  - 1 <= strs.length <= 104
+  - 1 <= strs.length <= 10^4
   - 0 <= strs[i].length <= 100
   - strs[i] consists of lowercase English letters.
 
